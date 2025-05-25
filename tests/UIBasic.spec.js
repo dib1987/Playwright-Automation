3e5cd9aef5d55388a9afc79060549b8b59b44d7a
dib1987 <43892634+dib1987@users.noreply.github.com>
2025-05-25 15:12:22 -0400

    Negative tests

--- tests/UIBasic.spec.js
+++ tests/UIBasic.spec.js
@@ -1,15 +1,19 @@
 const {test, expect} =require('@playwright/test');
 
-test('browser context playwright test',async ({browser})=>
+test('Validate if text is present on the Practice page',async ({browser})=>
 {
 const context = await browser.newContext();
 const page = await context.newPage();
 await page.goto("https://rahulshettyacademy.com/practice-project");
-
-
+await page.locator('#name').fill("Dibyendu Mondal");
+await page.locator('#email').fill("dibyendumondal87@gmail.com");
+await page.locator('#form-submit').click();
+console.log = (await page.locator('.section-title.h1'));
+const actualText = await page.locator('.section-title.h1').textContent();
+await expect(actualText).toContain('OUR PROJECTS');
 
 });
-test.only('page playwright test',async ({page})=>
+test('page playwright test',async ({page})=>
 {
 
 await page.goto("https://google.com");
@@ -19,3 +23,19 @@ await expect(page).toHaveTitle("Google")
 
 });
 
+test.only('Validate Negative Scenario  present on the Practice page',async ({browser})=>
+{
+const context = await browser.newContext();
+const page = await context.newPage();
+await page.goto("https://rahulshettyacademy.com/practice-project");
+await page.locator('#name').fill("Dibyendu Mondal");
+// await page.locator('#email').fill("dibyendumondal87@gmail.com");
+await page.locator('#form-submit').click();
+    const emailInput = await page.locator('#email');
+    const emailValidation = await emailInput.evaluate(el => el.validationMessage);
+    console.log(`Email Field Validation Message: ${emailValidation}`);
+    expect(emailValidation).toBe("Please fill out this field.");
+
+
+
+});
